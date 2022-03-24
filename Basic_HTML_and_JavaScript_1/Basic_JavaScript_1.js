e6180148c9e638f91f53cad471ea3cfc6b2af87b
anngansit <100404019+anngansit@users.noreply.github.com>
2022-03-23 23:47:38 -0700

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+	<script 
+	alert("Hello, World!");
+	</script>
+	
+	
+	<script>
+	function My_First_Function() { var String="KIss me, I'm Irish!";
+	var result=String.fontcolor("green"); document.getElementById("Irish").innerHTML=result;
+	}
+	</script>
+	
+</html>
